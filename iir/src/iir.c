1272786666dfd39e91fda678f1df5527f2d66094
Barabas Raffai <barabas.raffai@gmail.com>
2021-01-03 14:49:49 +0000

    Replaced with custom TDF-2 implementation

--- iir/src/iir.c
+++ iir/src/iir.c
@@ -4,35 +4,36 @@
 #include <stdio.h>
 #endif
 
-//from https://stackoverflow.com/questions/50588879/how-to-implement-iir-filter-in-c
+float get_state_term(int i, float *w, int coeff_len)
+{
+    i += 1;
+    if( i < coeff_len/2 - 1 )
+    {
+        return w[i];
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 void iir_process(const float *input, float *output, int len, float *coef, float *w, int coeff_len)
 {
-    int filterLength = coeff_len/2;
-
-    float *out = output;
-    const float *in = input;
+    float tmp;
     float *b = coef;
-    float *a = &coef[filterLength];
+    float *a = &coef[coeff_len/2];
+    for(int i = 0; i < len; i++)
+    {
+        //calculate output
+        output[i] = (w[0] + b[0] * input[i])/a[0];
 
-
-    const float a0 = a[0];
-    const float *a_end = &a[filterLength-1];
-    const float *out_start = out;
-    a++;
-    out--;
-    size_t m;
-    for (m = 0; m < len; m++) {
-        const float *b_macc = b;
-        const float *in_macc = in;
-        const float *a_macc = a;
-        const float *out_macc = out;
-        float b_acc = (*in_macc--) * (*b_macc++);
-        float a_acc = 0;
-        while (a_macc <= a_end && out_macc >= out_start) {
-            b_acc += (*in_macc--) * (*b_macc++);
-            a_acc += (*out_macc--) * (*a_macc++);
+        for(int j = 0; j < coeff_len/2; j++)
+        {
+            //update the state
+            w[j] = 0;
+            w[j] -= a[j+1] * output[i];
+            w[j] += b[j+1] * input[i];
+            w[j] += get_state_term(j, w, coeff_len);
         }
-        *++out = (b_acc - a_acc) / a0;
-        in++;
     }
 }
